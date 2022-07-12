c95a168e3233704678ad2a400b6fb4c97768bd6b
Mikko Johansson <mikko.johansson2@gmail.com>
2022-07-12 15:40:26 +0300

    App.js created and working

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,12 +2,14 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (req, res) => {
+  console.log('GET')
   Blog.find({}).then((blogs) => {
     res.json(blogs)
   })
 })
 
 blogsRouter.post('/', (req, res) => {
+  console.log('POST')
   const blog = new Blog(req.body)
 
   blog.save().then((newBlog) => {
